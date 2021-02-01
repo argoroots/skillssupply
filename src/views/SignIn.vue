00b2640f6fd1a78b898ed3bbd7156b240ce3ce62
Argo Roots <argo.roots@adm.ee>
2021-02-01 12:43:21 +0200

    Add Sign In view

---
+++ src/views/SignIn.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="signin">
+    <h1>This is an about page</h1>
+    <amplify-authenticator>
+    <!-- The rest of your app code -->
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
+  </div>
+</template>
